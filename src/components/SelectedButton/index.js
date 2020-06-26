046b2391cc1f2174788ca7c844b5366e94c977c1
João Manoel Neto <joaaomanooel@gmail.com>
2020-06-26 15:34:58 -0300

    add index to components

--- src/components/SelectedButton/index.js
+++ src/components/SelectedButton/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { TouchableWithoutFeedback } from 'react-native';
+import { colors } from '@/constants';
 
 import { Button } from './styles';
 
 export default ({ onPress, name, lib: Icon }) => {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
-      <Button>
-        <Icon name={name} size={24} color="#FFF" />
-      </Button>
+      <Button><Icon name={name} size={24} color={colors.lightWhite()} /></Button>
     </TouchableWithoutFeedback>
   );
 };
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+module.exports = ({
+  SelectedButton: require('./SelectedButton').default,
+});
