e58edbab4e4372129b6c546d7030782d10b0b7f4
João Manoel Neto <joaaomanooel@gmail.com>
2020-06-30 18:11:34 -0300

    update component exports

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ module.exports = ({
   SelectedButton: require('./SelectedButton').default,
   AnimatedButton: require('./AnimatedButton').default,
   CardWithImage: require('./CardWithImage').default,
+  AnimatedView: require('./AnimatedView').default,
   Notification: require('./Notification').default,
   Button: require('./Button').default,
   Modal: require('./Modal').default,
