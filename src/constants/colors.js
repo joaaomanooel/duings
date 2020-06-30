892950b9ffd8e2adde08af6c7a5ac88d3fcab63a
João Manoel Neto <joaaomanooel@gmail.com>
2020-06-29 23:40:40 -0300

    update colors constants

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,8 +1,9 @@
 export default {
+  red: (op = 1) => `rgba(191, 46, 33, ${op})`,
   black: (op = 1) => `rgba(51, 51, 51, ${op})`,
-  yellow: (op = 1) => `rgba(238, 197, 80, ${op})`,
-  lightYellow: (op = 1) => `rgba(255, 237, 80, ${op})`,
-
+  yellow: (op = 1) => `rgba(255, 216, 54, ${op})`,
+  lightYellow: (op = 1) => `rgba(255, 237, 0, ${op})`,
+  green: (op = 1) => `rgba(0, 114, 63, ${op})`,
   white: (op = 1) => `rgba(240, 240, 240, ${op})`,
   darkWhite: (op = 1) => `rgba(233, 233, 233, ${op})`,
   lightWhite: (op = 1) => `rgba(255, 255, 255, ${op})`,
