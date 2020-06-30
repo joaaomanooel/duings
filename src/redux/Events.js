4df5feb7dfab7107037bbedfac85c862d9fccb0a
João Manoel Neto <joaaomanooel@gmail.com>
2020-06-30 20:39:07 -0300

    update events redux

--- src/redux/Events.js
+++ src/redux/Events.js
@@ -1,23 +1,19 @@
 import { createAction, handleActions } from 'redux-actions';
 
-export const getEventsRequest = createAction('GET_EVENTS_REQUEST');
-export const getEventsSuccess = createAction('GET_EVENTS_SUCCESS');
-export const getEventsFailure = createAction('GET_EVENTS_FAILURE');
+export const getAllEventsRequest = createAction('GET_ALL_EVENTS_REQUEST');
+export const getAllEventsSuccess = createAction('GET_ALL_EVENTS_SUCCESS');
+export const getAllEventsFailure = createAction('GET_ALL_EVENTS_FAILURE');
 
-export const addEventsRequest = createAction('ADD_EVENTS_REQUEST');
-export const addEventsSuccess = createAction('ADD_EVENTS_SUCCESS');
-export const addEventsFailure = createAction('ADD_EVENTS_FAILURE');
-
-export const updateEventsRequest = createAction('UPDATE_EVENTS_REQUEST');
-export const updateEventsSuccess = createAction('UPDATE_EVENTS_SUCCESS');
-export const updateEventsFailure = createAction('UPDATE_EVENTS_FAILURE');
-
-export const removeEventsRequest = createAction('REMOVE_EVENTS_REQUEST');
-export const removeEventsSuccess = createAction('REMOVE_EVENTS_SUCCESS');
-export const removeEventsFailure = createAction('REMOVE_EVENTS_FAILURE');
+export const checkinRequest = createAction('CHECKIN_REQUEST');
+export const checkinFailure = createAction('CHECKIN_FAILURE');
 
 const INITIAL_STATE = {
   data: [],
 };
 
-export default handleActions({}, INITIAL_STATE);
+export default handleActions({
+  [getAllEventsSuccess]: (state, { payload }) => ({
+    ...state,
+    data: payload,
+  }),
+}, INITIAL_STATE);
