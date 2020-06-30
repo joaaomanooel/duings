c39c7cbef75091fe1419d35c0cada4c558053f61
João Manoel Neto <joaaomanooel@gmail.com>
2020-06-30 18:15:02 -0300

    add redux provider on startup app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import createStore from '@/redux';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
-import { colors } from '@/constants';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import Navigation from './navigation/StackNavigation';
+
+const { store, persistor } = createStore();
+
+export { store };
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" translucent animated backgroundColor={colors.midWhite(0)} />
-      <Navigation />
+      <StatusBar barStyle="dark-content" translucent animated backgroundColor="transparent" />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
