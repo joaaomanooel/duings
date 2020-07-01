142b9e587c749cc02f6077115a08cb40a008af23
João Manoel Neto <joaaomanooel@gmail.com>
2020-07-01 17:18:35 -0300

    add location to user redux

--- src/redux/User.js
+++ src/redux/User.js
@@ -2,8 +2,14 @@ import { createAction, handleActions } from 'redux-actions';
 
 export const setUser = createAction('SET_USER');
 
-const INITIAL_STATE = { data: {} };
+export const getLocationRequest = createAction('SET_LOCATION');
+export const getLocationSuccess = createAction('SET_LOCATION');
+export const getLocationFailure = createAction('SET_LOCATION');
+
+const INITIAL_STATE = { data: {}, location: { city_district: '...' } };
 
 export default handleActions({
+  [getLocationSuccess]: (state, { payload }) => ({ ...state, location: payload }),
+  [getLocationFailure]: state => ({ ...state, location: INITIAL_STATE.location }),
   [setUser]: (state, { payload }) => ({ ...state, data: payload }),
 }, INITIAL_STATE);
